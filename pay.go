c3430bb6112e462a07cf3ad92b7209c5f49429cf
nimo@rain-tech.net <nimochu@126.com>
2019-09-23 13:55:07 +0800

    :tada: 统一下单

--- pay.go
+++ pay.go
@@ -1,15 +1,27 @@
 package gwechat
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/xml"
 	"fmt"
-	grand "github.com/og/go-rand"
-	"log"
+	ge "github.com/og/go-error"
+	gconv "github.com/og/x/conv"
+	glist "github.com/og/x/list"
+	gmap "github.com/og/x/map"
+	grand "github.com/og/x/rand"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"net/http"
 	"net/url"
+	"reflect"
 	"strings"
 )
 
+type PayErrBase struct {
+	ReturnCode string `xml:"return_code"`
+	ReturnMsg string `xml:"return_msg"`
+}
 type PayErrRes struct {
 	Fail bool
 	Msg string
@@ -20,7 +32,8 @@ func (payErrRes *PayErrRes) SetError(err error) {
 	payErrRes.Msg = err.Error()
 	payErrRes.Error = err
 }
-type PayUnifiedorderMustData struct {
+type PayUnifiedOrderQuery struct {
+	x string `xml:"-";note:"可选参数开始"`
 	APPID 			string `xml:"appid";____________note:"不读取config 中的appid，因为一个商户号可以向不同的小程序进行支付"`
 	OutTradeNo 		string `xml:"out_trade_no";_____note:"商户订单号"`
 	TotalFee 		int    `xml:"total_fee";________note:"支付金额。注意是以分为单位"`
@@ -29,8 +42,7 @@ type PayUnifiedorderMustData struct {
 	NotifyURL	    string `xml:"notify_url";_______note:"通知地址"`
 	TradeType 		string `xml:"trade_type";_______note:"交易类型"`
 	OpenID			string `xml:"openid";___________note:"trade_type=JSAPI 时候必传"`
-}
-type PayUnifiedorderOptionalData struct {
+	xx string `xml:"-";note:"可选参数结束"`
 	DeviceInfo string `xml:"device_info"`
 	Attach string `xml:"attach"`
 	TimeStart string `xml:"time_start"`
@@ -40,73 +52,107 @@ type PayUnifiedorderOptionalData struct {
 	LimitPay string `xml:"limit_pay"`
 	Receipt string `xml:"receipt"`
 	Detail string `xml:"detail"`
-	SceneInfo struct{
-		ID string `xml:"id"`
-		Name string `xml:"name"`
-		AreaCode string `xml:"area_code"`
-		Address string `xml:"address"`
-	}
+	XMLName  xml.Name `xml:"xml"`
+	FeeType string `xml:"fee_type"`
+	xxx string `xml:"-";note:"内部属性不要配置 Start"`
+	MCHID string `xml:"mch_id"`
+	MCHKey string `xml:"-"`
+	NonceStr string `xml:"nonce_str"`
+	Sign string `xml:"sign"`
+	xxxx string `xml:"-";note:"内部属性不要配置 End"`
+
+}
+
+type UnifedOrderResult struct {
+	ResultCode string `xml:"result_code"`
+	ResultMsg string `xml:"return_msg"`
+	CodeURL string `xml:"code_url"`
+	PrepayID string `xml:"prepay_id"`
+	TradeType string `xml:"trade_type"`
+	ErrCode string `xml:"err_code"`
+	ErrCodeDes string `xml:"err_code_des"`
+	APPID string `xml:"appid"`
+	MCHID string `xml:"mch_id"`
+	DeviceInfo string `xml:"device_info"`
+	NonceStr string `xml:"nonce_str"`
+	Sign string `xml:"sign"`
+
 }
 // https://pay.weixin.qq.com/wiki/doc/api/wxa/wxa_api.php?chapter=9_1
 // 统一下单
-func (this Wechat) PayUnifiedorder (mustData PayUnifiedorderMustData, optionalData PayUnifiedorderOptionalData) (payErrRes PayErrRes)  {
+func (this Wechat) PayUnifiedOrder (query PayUnifiedOrderQuery) (result UnifedOrderResult, payErrRes PayErrRes)  {
+	query.MCHID = this.mchID
+	query.MCHKey = this.mchKey
+	query.NonceStr = grand.StringLetter(32)
 
-	query := struct {
-		XMLName  xml.Name `xml:"xml"`
-		PayUnifiedorderMustData
-		PayUnifiedorderOptionalData
-		MCHID string `xml:"mch_id"`
-		NonceStr string `xml:"nonce_str"`
-		SignType string `xml:"sign_type"`
-		Sign string `xml:"sign"`
-		FeeType string `xml:"fee_type"`
-	}{
-		PayUnifiedorderMustData: mustData,
-		PayUnifiedorderOptionalData: optionalData,
-		MCHID: this.mchID,
-		NonceStr: grand.StringLetter(32),
-		SignType: dict.PayUnifiedorder.SignType.MD5,
-		FeeType: dict.PayUnifiedorder.FeeType.CNY,
-	}
-	query.Sign = CreatePaySign(PaySignSource{
-		APPID: query.APPID,
-		MCHID: this.mchID,
-		MCHKey: this.mchKey,
-		Body: query.Body,
-		NonceStr: query.NonceStr,
-		DeviceInfo: query.DeviceInfo,
-	})
-
+	query.Sign = CreatePaySign(query)
 	xmlByteList, err := xml.Marshal(query)
 	if err !=nil {
 		payErrRes.SetError(err)
 		return
 	}
-	xmlString := string(xmlByteList[:])
-	log.Print("query.Sign ", query.Sign)
-	_=xmlString
+	client := &http.Client{}
+	requestBody :=  bytes.NewReader([]byte{})
+	requestBody = bytes.NewReader(xmlByteList)
+	request, err := http.NewRequest("POST", "https://api.mch.weixin.qq.com/pay/unifiedorder", requestBody)
+	if err !=nil {
+		payErrRes.SetError(err)
+		return
+	}
+	request.Header.Set("Content-Type", "text/xml")
+	response, err := client.Do(request) ; if err !=nil { payErrRes.SetError(err) ; return }
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body) ; if err !=nil { payErrRes.SetError(err) ; return }
+	err = xml.Unmarshal(body, &result) ; ge.Check(err)
+	if result.ResultCode != "SUCCESS" {
+		payErrRes.SetError(errors.New(result.ResultMsg))
+		return
+	}
+	if result.ResultCode != "SUCCESS" {
+		payErrRes.SetError(errors.New(result.ErrCodeDes))
+		return
+	}
 	return
 }
 
-type PaySignSource struct {
-	APPID string
-	MCHID string
-	MCHKey string
-	Body string
-	NonceStr string
-	DeviceInfo string
-}
-func CreatePaySign(data PaySignSource) (sign string) {
-	signURLValues := url.Values{
-		"appid": {data.APPID},
-		"mch_id": {data.MCHID},
-		"body": {data.Body},
-		"nonce_str": {data.NonceStr},
+func CreatePaySign(query PayUnifiedOrderQuery) (sign string) {
+	queryValues := url.Values{}
+	value := reflect.ValueOf(query)
+	valueType := reflect.TypeOf(query)
+	itemLen := value.NumField()
+	for i:=0;i<itemLen;i++ {
+		queryKey := reflect.StructTag(valueType.Field(i).Tag).Get("xml")
+		switch queryKey{
+			case "-":
+				continue
+			case "":
+				panic("need tag name:" + valueType.Field(i).Name)
+		}
+		valueItem := value.Field(i)
+		queryValue := ""
+		switch valueItem.Type().String() {
+		case "string":
+			queryValue = valueItem.String()
+		case "int":
+			queryValue = gconv.Int64String(valueItem.Int())
+		case "float64":
+			queryValue = fmt.Sprintf("%f", valueItem.Float())
+		case "xml.Name":
+			continue
+		default:
+			panic("error type:" + valueItem.Type().String())
+		}
+		if queryValue != "" {
+			queryValues.Set(queryKey, queryValue)
+		}
 	}
-	if data.DeviceInfo != "" {
-		signURLValues.Set("device_info", data.DeviceInfo)
+	keyList := gmap.Keys(queryValues).String()
+	queryStringList := glist.StringList{}
+	for i:=0;i<len(keyList);i++ {
+		key := keyList[i]
+		queryStringList.Push(key + "=" + queryValues.Get(key))
 	}
-	md5Byte := md5.Sum([]byte(signURLValues.Encode() + "&key=" + data.MCHKey))
+	md5Byte := md5.Sum([]byte(queryStringList.Join("&") + "&key=" + query.MCHKey))
 	sign = strings.ToUpper(fmt.Sprintf("%x", md5Byte))
 	return
 }
--- pay_test.go
+++ pay_test.go
@@ -1,9 +1,11 @@
 package gwechat_test
 
 import (
+	gjson "github.com/og/go-json"
 	grand "github.com/og/go-rand"
 	gwechat "github.com/og/go-wechat"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 )
 
@@ -13,8 +15,7 @@ var someMCH = gwechat.New(gwechat.Config{
 })
 
 func TestWechat_PayUnifiedorder(t *testing.T) {
-
-	payErrRes := someMCH.PayUnifiedorder(gwechat.PayUnifiedorderMustData{
+	payResult, payErrRes := someMCH.PayUnifiedOrder(gwechat.PayUnifiedOrderQuery{
 		APPID: "wx9f01246e31fd5cae",
 		OutTradeNo: grand.StringLetter(32),
 		TotalFee: 1,
@@ -23,23 +24,32 @@ func TestWechat_PayUnifiedorder(t *testing.T) {
 		NotifyURL: "https://github.com/og/go-wechat",
 		TradeType: gwechat.Dict().PayUnifiedorder.TradeType.JSAPI,
 		OpenID: "otU685kfo1D-tAbgaNVqbPiiRd8k",
-	}, gwechat.PayUnifiedorderOptionalData{})
+	})
 	if payErrRes.Fail {
 		panic(payErrRes.Error)
 	}
+	log.Print(gjson.StringUnfold(payResult))
 }
 // https://pay.weixin.qq.com/wiki/doc/api/jsapi.php?chapter=20_1
 func TestCreatePaySign(t *testing.T) {
-	assert.Equal(t, `F7F63040EE22E4D84905EB3CF9B35C7E`, gwechat.CreatePaySign(gwechat.PaySignSource{
+
+	assert.Equal(t, `40DE1DF376357F4F657DBCCACF73223F`, gwechat.CreatePaySign(gwechat.PayUnifiedOrderQuery{
 		APPID: "wx9111246221fd5cae",
-		MCHID: "1112225871",
-		MCHKey: "wxda123461113e2357wxdac12521113e",
 		Body: "body",
-		NonceStr: "SrwzbDPKqTOsHkVxzjtTXOnrMrPedtcO",
 		DeviceInfo: "info",
+		MCHID: "1112225871",
+		NonceStr: "SrwzbDPKqTOsHkVxzjtTXOnrMrPedtcO",
+	}))
+	assert.Equal(t, `048DEBED0793A789B6D3AA4D002170D5`, gwechat.CreatePaySign(gwechat.PayUnifiedOrderQuery{
+		APPID: "wx9111246221fd5cae",
+		Body: "body",
+		DeviceInfo: "info",
+		MCHID: "1112225871",
+		NonceStr: "SrwzbDPKqTOsHkVxzjtTXOnrMrPedtcO",
+		NotifyURL: "http://github.com/og/go-wechat",
 	}))
 
-	assert.Equal(t, `316C621C950CC4222D905C463185DDF7`, gwechat.CreatePaySign(gwechat.PaySignSource{
+	assert.Equal(t, `687EC6E0AFCDBFB825E05B4EA83103BF`, gwechat.CreatePaySign(gwechat.PayUnifiedOrderQuery{
 		APPID: "wx9111246221fd5cae",
 		MCHID: "1112225871",
 		MCHKey: "wxda123461113e2357wxdac12521113e",
